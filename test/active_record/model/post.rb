36a0f5fdb9a975ee6ba5d5447baf08aaa2c57683
Fumihiro Ito <fmhrit@gmail.com>
2016-08-04 11:43:51 +0900

    Backport: fetch_by_uniq_keys

--- test/active_record/model/post.rb
+++ test/active_record/model/post.rb
@@ -1,8 +1,10 @@
 # -*- encoding : utf-8 -*-
 ActiveRecord::Base.connection.create_table(:posts, :force => true) do |t|
   t.text  :body
+  t.string :slug
   t.integer :topic_id
 end
+ActiveRecord::Base.connection.add_index(:posts, %i{topic_id slug}, unique: true)
 
 class Post < ActiveRecord::Base
   acts_as_cached
